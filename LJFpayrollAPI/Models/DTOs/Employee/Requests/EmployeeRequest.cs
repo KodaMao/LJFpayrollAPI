1cbe78e23e24ede87c1f1ec3a9ecae52daaf6ed6
kodamao <engrleonelfelezario812@gmail.com>
2026-03-31 15:55:24 +0800

    create employee DTOs

---
+++ LJFpayrollAPI/Models/DTOs/Employee/Requests/EmployeeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace LJFpayrollAPI.Models.DTOs.Employee.Requests
+{
+    public class EmployeeRequest
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? MiddleName { get; set; }
+        public required DateOnly DateOfBirth { get; set; }
+        public required decimal DailyRate { get; set; }
+        public required string WorkingDays { get; set; }
+    }
+}
---
+++ LJFpayrollAPI/Models/DTOs/Employee/Responses/EmployeeResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace LJFpayrollAPI.Models.DTOs.Employee.Responses
+{
+    public class EmployeeResponse
+    {
+        public int Id { get; set; }
+        public required string EmployeeNumber { get; set; }
+        public required string FullName { get; set; }
+        public required DateOnly DateOfBirth { get; set; }
+        public required decimal DailyRate { get; set; }
+        public required string WorkingDays { get; set; }
+    }
+}
