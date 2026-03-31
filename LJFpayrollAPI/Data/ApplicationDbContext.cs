e80d6a8242f6940490e302612198838b6ec0d4f5
kodamao <engrleonelfelezario812@gmail.com>
2026-03-31 15:54:05 +0800

    create db context and employee entity

---
+++ LJFpayrollAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,52 @@
+﻿using LJFpayrollAPI.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace LJFpayrollAPI.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Employee>(entity =>
+            {
+                entity.ToTable("Employees");
+
+                entity.HasKey(e => e.Id);
+
+                entity.Property(e => e.EmployeeNumber)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasIndex(e => e.EmployeeNumber)
+                    .IsUnique();
+
+                entity.Property(e => e.FirstName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.LastName).IsRequired().HasMaxLength(100);
+                entity.Property(e => e.MiddleName).HasMaxLength(100);
+
+                entity.Property(e => e.DateOfBirth)
+                    .IsRequired()
+                    .HasColumnType("date");
+
+                entity.Property(e => e.DailyRate)
+                    .IsRequired()
+                    .HasPrecision(18, 2);
+
+
+                entity.Property(e => e.WorkingDays)
+                    .IsRequired()
+                    .HasMaxLength(10);
+
+                entity.Property(e => e.IsDeleted)
+                    .IsRequired()
+                    .HasDefaultValue(false);
+
+                entity.HasQueryFilter(e => !e.IsDeleted);
+            });
+        }
+    }
+}
---
+++ LJFpayrollAPI/Models/Entities/Employee.cs
@@ -0,0 +1,23 @@
+﻿namespace LJFpayrollAPI.Models.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public required string EmployeeNumber { get; set; } 
+
+        public required string FirstName { get; set; }
+
+        public required string LastName { get; set; }
+
+        public string? MiddleName { get; set; }
+
+        public required DateOnly DateOfBirth { get; set; }
+
+        public required decimal DailyRate { get; set; }
+
+        public required string WorkingDays { get; set; } 
+
+        public bool IsDeleted { get; set; } = false; 
+    }
+}
